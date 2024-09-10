82078e895aeb79ed5aaff53172311b7512abcc3a
Jose Real <jrealmorillo@gmail.com>
2024-09-10 17:37:42 +0200

    deployed app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Full-Stack-Open-Part-3
 This is my repo for Part 3 of [Full Stack Open Course](https://fullstackopen.com/) by [University of Helsinki](https://www.helsinki.fi/en)
+
+Link to deployed App: https://full-stack-open-part-3-wild-fog-3703.fly.dev/
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
+app.use(express.static('dist'))
+
 
 morgan.token("personName", function (req, res) {
   return req.body.name || ''; 
@@ -103,6 +107,7 @@ app.post("/api/persons/", (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
