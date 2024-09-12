efd5699d6b160f2b5d9257409b4bcbacbec50014
Jose Real <jrealmorillo@gmail.com>
2024-09-12 14:30:26 +0200

    added Mongo DB

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+    console.log("give password as argument")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url = `mongodb+srv://FSOpen:${password}@cluster0.5ze7j.mongodb.net/PhonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set("strictQuery", false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv.length > 3) {
+    const person = new Person({
+        name: newName,
+        number: newNumber,
+    })
+
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length === 3) {
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+
+
+
+
+
+
